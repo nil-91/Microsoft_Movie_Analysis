d14cb3b7ef148450295b79aaa6809dcc1faccbfb
nsaharkhiz <ns3149@columbia.edu>
2023-02-08 23:25:32 -0500

    finished visualization

--- code/visualizations.py
+++ code/visualizations.py
@@ -28,24 +28,10 @@ sns.set_style("white")
 
 
 def genre_vs_income(clean_df):
-    genre_vs_rating = clean_df[['averagerating','genre0']]
-
-    ax = genre_vs_rating.groupby('genre0').median().plot(kind='bar', figsize=(10,6), fontsize=13, stacked=True, 
-                                                    color = ['royalblue'])
-    ax.set_xlabel('Genre of the movie', fontsize = 15)
-    ax.set_ylabel("Average rating", fontsize=15);
-    ax.set_title("Average rating by movie type", fontsize=22)
-    plt.xticks(rotation = 50)
-    plt.legend(fontsize=12)
-    plt.savefig('./images/genre_vs_rating.png', transparent = True)   
-    plt.show()
-    pass
-
-def genre_vs_rating(clean_df):
     genre_vs_gross = clean_df[['domestic_gross','foreign_gross','genre0']]
 
-    ax = genre_vs_gross.groupby('genre0').median().plot(kind='bar', figsize=(10,6), fontsize=13, stacked=True, 
-                                                    color = ['lightcoral','royalblue'])
+    ax = genre_vs_gross.groupby('genre0').mean().plot(kind='bar', figsize=(10,6), fontsize=13, stacked=True, 
+                                                         color = ['#894da3','#8c7dba'])
     ax.set_xlabel('Genre of the movie', fontsize = 15)
     ax.set_ylabel("Income [$]", fontsize=15);
     ax.set_title("Gross income by movie type", fontsize=22)
@@ -53,77 +39,118 @@ def genre_vs_rating(clean_df):
     ax.yaxis.set_major_locator(ticker.MultipleLocator(50000000.00))
     ax.yaxis.set_major_formatter(ticker.EngFormatter())
     plt.xticks(rotation = 50)
-    plt.legend(fontsize=12)
-    plt.savefig('./images/genre_vs_income.png', transparent = True)   
+    plt.legend(labels=['Domestic gross', 'Foreign gross'], fontsize=12)
+    plt.savefig('./images/genre_vs_income.png', transparent = True)    
+    plt.show()
+    pass
+
+def genre_vs_rating(clean_df):
+    genre_vs_rating = clean_df[['averagerating','genre0','numvotes']]
+    df = genre_vs_rating.groupby('genre0').agg('median')
+    df = df.reset_index()
+    
+    fig, ax = plt.subplots(figsize=(15,6))
+    labels = list(df['genre0'].unique())
+    sns.barplot(
+        x="genre0",
+        y="averagerating",
+        data=df,
+        hue="numvotes",
+        ax=ax,
+        palette="BuPu_r",
+        dodge=False
+    )
+    sns.set_style(
+        "darkgrid")
+    ax.set_title("Average movie rating per category", fontsize=15)
+    ax.set_xlabel("Movie category", fontsize=15)
+    ax.set_ylabel("Average rating", fontsize=15)
+    ax.set_xticklabels(labels= labels, size=15, rotation=65)
+    ax.tick_params(axis='y', labelsize=15)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], title="Number of votes",bbox_to_anchor=(1.02, 1.0), loc='upper left', fontsize=12,
+              fancybox=True)
+    plt.tight_layout()
+    plt.savefig('./images/genre_vs_rating.png', transparent = True)    
+
     plt.show()
     pass
 
 
 def income_vs_year(clean_df): 
-    gross_vs_year = clean_df[['domestic_gross','foreign_gross','year']]
-    gross_vs_year['foreign_gross'].replace(',','', regex=True, inplace=True)
-    gross_vs_year['foreign_gross'] = genre_vs_gross['foreign_gross'].astype('float64')
+    income_vs_year = clean_df[['year','domestic_gross','foreign_gross']]
+    df = income_vs_year[['domestic_gross','year']].groupby('year').agg('count')
+    df.reset_index()
+    df.rename(columns={'domestic_gross':'count'}, inplace=True)
+    income_vs_year = pd.merge(income_vs_year, df, on='year')
 
-    ax = gross_vs_year.groupby('year').mean().plot(figsize=(10,6), fontsize=13, stacked=True, 
-                                                        color = ['lightcoral','royalblue'])
-    ax.set_xlabel('Year', fontsize = 15)
-    ax.set_ylabel("Income [$]", fontsize=15);
-    ax.set_title("Gross income by year", fontsize=22)
-    ax.yaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(20000000.00))
-    ax.yaxis.set_major_formatter(ticker.EngFormatter())
-    plt.xticks(rotation = 50)
-    plt.legend(fontsize=12)
-    plt.grid()
+    fig, axes = plt.subplots(figsize=(25,6), ncols=2)
+    sns.boxplot(x="year",
+                    y="domestic_gross",
+                    data=income_vs_year,
+                    ax=axes[0],
+                    palette="BuPu_r",
+                    hue="count",
+                    dodge=False
+    )
+    sns.boxplot(x="year",
+                    y="foreign_gross",
+                    data=income_vs_year,
+                    ax=axes[1],
+                    palette="BuPu_r",
+                    hue="count",
+                    dodge=False    
+    )
+    axes[0].set_title('Domestic gross across years', fontsize=15)
+    axes[0].set_xlabel('Year', fontsize=15)
+    axes[0].set_ylabel('Domestic gross [$]', fontsize=15)
+    axes[0].set_ylim((-50000, 970000000))
+    axes[0].tick_params(axis='y', labelsize=15)
+    axes[0].tick_params(axis='x', labelsize=15)
+    axes[0].yaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
+    axes[0].yaxis.set_major_locator(ticker.MultipleLocator(100000000.00))
+    axes[0].yaxis.set_major_formatter(ticker.EngFormatter())
+    axes[0].legend(title="Number of movies",loc=2, fontsize=15, fancybox=True)
+    
+    axes[1].set_title('Foreign gross across years', fontsize=15)
+    axes[1].set_xlabel('Year', fontsize=15)
+    axes[1].set_ylabel('Foreign gross [$]', fontsize=15)
+    axes[1].set_ylim((-50000, 970000000))
+    axes[1].tick_params(axis='y', labelsize=15)
+    axes[1].tick_params(axis='x', labelsize=15)
+    axes[1].yaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
+    axes[1].yaxis.set_major_locator(ticker.MultipleLocator(100000000.00))
+    axes[1].yaxis.set_major_formatter(ticker.EngFormatter())
+    axes[1].legend().remove()
     plt.savefig('./images/income_vs_year.png', transparent = True)   
     plt.show()
     pass
 
-def votes_vs_year(clean_df): 
-    votes_vs_year = clean_df[['numvotes','year']]
-
-    ax = votes_vs_year.groupby('year').mean().plot(figsize=(10,6), fontsize=13, stacked=True, 
-                                                        color = ['lightcoral','royalblue'])
-    ax.set_xlabel('Year', fontsize = 15)
-    ax.set_ylabel("Number of votes", fontsize=15);
-    ax.set_title("Total votes by year", fontsize=22)
+def income_vs_rating(clean_df): 
+    income_vs_vote = clean_df[['averagerating','total_gross','numvotes']]
+    fig, ax = plt.subplots(figsize=(15,6))
+    sns.scatterplot(
+                    x="averagerating",
+                    y="total_gross",
+                    data=income_vs_vote,
+                    alpha=0.9,
+                    color=['#894da3']
+    )
+    ax.set_title('Total gross with repect to average rating for all the movies', fontsize=15)
+    ax.set_xlabel('Average rating', fontsize=15)
+    ax.set_ylabel('Total gross [$]', fontsize=15)
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=15)
     ax.yaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(20000000.00))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(100000000.00))
     ax.yaxis.set_major_formatter(ticker.EngFormatter())
-    plt.xticks(rotation = 50)
-    plt.legend(fontsize=12)
-    plt.grid()
-    plt.savefig('./images/votes_vs_year.png', transparent = True)   
+    ax.legend().remove()
+    ax.autoscale_view()
+    plt.tight_layout()
+    plt.savefig('./images/income_vs_rating.png', transparent = True)   
     plt.show()
     pass
 
-def sample_plot2():
-    """
-    This is a sample visualization function to show what one looks like.
-    The code is borrowed from https://www.machinelearningplus.com/plots/top-50-matplotlib-visualizations-the-master-plots-python/
-
-    This function takes no arguments and shows a nice visualization without having all your code in the notebook itself.
-    """
-
-    plt.figure(figsize=(16, 10), dpi=80)
-    # Import Data
-    df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
-
-    # Draw Plot
-    plt.figure(figsize=(16,10), dpi= 80)
-    sns.kdeplot(df.loc[df['cyl'] == 4, "cty"], shade=True, color="g", label="Cyl=4", alpha=.7)
-    sns.kdeplot(df.loc[df['cyl'] == 5, "cty"], shade=True, color="deeppink", label="Cyl=5", alpha=.7)
-    sns.kdeplot(df.loc[df['cyl'] == 6, "cty"], shade=True, color="dodgerblue", label="Cyl=6", alpha=.7)
-    sns.kdeplot(df.loc[df['cyl'] == 8, "cty"], shade=True, color="orange", label="Cyl=8", alpha=.7)
-
-    # Decoration
-    plt.title('Density Plot of City Mileage by n_Cylinders', fontsize=22)
-    plt.gca().set(xlabel='Mileage per Gallon in the City', ylabel='Kernel Denisty')
-    plt.legend()
-    plt.savefig('./images/viz2.png', transparent = True)
-    plt.show()
-
-    pass
 
 
 def visualization_one(target_var = None, input_vars= None, output_image_name=None):
